187c473dbfa0c80d99c781973a501e94c6344813
Carlos Matheus <carlos.matheus.10@hotmail.com>
2019-05-02 10:24:39 -0300

    create pages about  contact e home

--- django_ecomerce/urls.py
+++ django_ecomerce/urls.py
@@ -20,7 +20,7 @@ from .views import home_page,about_page,contact_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_page),
-    path('about/', about_page()),
-    path('contact/', contact_page()),
+    path('',home_page),
+    path('about/', about_page),
+    path('contact/', contact_page),
 ]
